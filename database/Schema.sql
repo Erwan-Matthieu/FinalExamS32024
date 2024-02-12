14943cdecc3ccabc9ef542181d82ada5d6a7151b
Erwan-Matthieu <52382415+Erwan-Matthieu@users.noreply.github.com>
2024-02-12 15:43:23 +0300

    Update function, schema, view and To-Do List

--- database/Schema.sql
+++ database/Schema.sql
@@ -8,3 +8,38 @@ CREATE TABLE serenitea_farms_utilisateurs (
     date_d_inscription DATE NOT NULL,
     admin BOOLEAN DEFAULT false,
 );
+
+CREATE TABLE serenitea_farms_the (
+    variete VARCHAR(255) PRIMARY KEY,
+    occupation DOUBLE DEFAULT 1,
+    rendement DOUBLE DEFAULT 1
+);
+
+CREATE TABLE serenitea_farms_parcelles (
+    reference VARCHAR(20) PRIMARY KEY,
+    surface DOUBLE DEFAULT 1,
+    variete VARCHAR(255) NOT NULL,
+    Foreign Key serenitea_farms_parcelles(variete) REFERENCES serenitea_farms_the(variete)
+);
+
+CREATE TABLE serenitea_farms_ceuilleurs (
+    id_ceuilleur VARCHAR(20) PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    date_de_naissance DATE NOT NULL,
+    date_d_embauche DATE DEFAULT now(),
+    date_de_depart DATE DEFAULT NULL
+);
+
+CREATE TABLE serenitea_farms_depenses(
+    nom VARCHAR(255) NOT NULL,
+    date DATETIME DEFAULT now(),
+    montant DOUBLE DEFAULT 0
+);
+
+CREATE TABLE serenitea_farms_historique_salaire (
+    id_ceuilleur VARCHAR(20),
+    date DATE DEFAULT now(),
+    montant DOUBLE DEFAULT 0,
+    Foreign Key serenitea_farms_historique_salaire(id_ceuilleur) REFERENCES serenitea_farms_ceuilleurs(id_ceuilleur)
+);
---
+++ database/View.sql
@@ -0,0 +1,21 @@
+CREATE VIEW v_serenitea_farms_salaires AS
+SELECT
+    sfc.id_ceuilleur
+    sfhs.montant AS salaire
+FROM
+    serenitea_farms_ceuilleurs sfc
+JOIN 
+    serenitea_farms_historique_salaire sfhs
+    ON sfc.id_ceuilleur = sfhs.id_ceuilleur
+JOIN
+    (
+        SELECT
+            id_ceuilleur,
+            montant
+        FROM
+            serenitea_farms_historique_salaire
+        GROUP BY
+            id_ceuilleur
+    ) AS recent_salary rs
+ON
+    sfc.id_ceuilleur = rs.id_ceuilleur;
