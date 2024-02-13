59563c49717febcf9705084b3de7b8d4e25a8728
Erwan-Matthieu <52382415+Erwan-Matthieu@users.noreply.github.com>
2024-02-13 08:04:20 +0300

    Update

---
+++ pages/Login.php
@@ -0,0 +1,125 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   
+    </style>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Login - Brand</title>
+        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
+        <link rel="stylesheet" href="../assets/fonts/fontawesome-all.min.css">
+    </head>
+    
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Pages / Login - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Nov 17 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="bg-gradient-primary">
+    <div id="tout">
+        <div class="row justify-content-center">
+            
+    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <img src="../assets/img/logo.png" alt="">
+                  <span class="d-none d-lg-block" _msttexthash="793767" _msthash="1">SenereTea Farms</span>
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4" _msttexthash="740844" _msthash="2">Connectez-vous à votre compte</h5>
+                    <p class="text-center small" _msttexthash="5534230" _msthash="3">Entrez votre nom d’utilisateur et votre mot de passe pour vous connecter</p>
+                  </div>
+
+                  <form class="row g-3 needs-validation" method="post" novalidate="">
+
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label" _msttexthash="1486862" _msthash="4">Nom d’utilisateur</label>
+                      <div class="input-group has-validation" _msthidden="1">
+                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                        <input type="text" name="username" class="form-control" id="yourUsername" required="">
+                        <div class="invalid-feedback" _msttexthash="601653" _msthidden="1" _msthash="5">Please enter your username.</div>
+                      </div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="yourPassword" class="form-label" _msttexthash="157794" _msthash="6">Mot de passe</label>
+                      <input type="password" name="password" class="form-control" id="yourPassword" required="">
+                      <div class="invalid-feedback" _msttexthash="603863" _msthidden="1" _msthash="7">Please enter your password!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
+                        <label class="form-check-label" for="rememberMe" _msttexthash="383331" _msthash="8">Souvenez-vous de moi</label>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit" _msttexthash="256685" _msthash="9">Connectez-vous</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0" _msttexthash="2468271" _msthash="10">Vous n’avez pas de compte ? <a href="pages-register.html" _istranslated="1">Créer un compte</a></p>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ --><font _mstmutation="1" _msttexthash="495235" _msthash="11">Veuillez completer tout les champs s'il vous plait</font>
+              </div>
+              </div>
+            </div>     
+</div>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/bs-init.js"></script>
+<script src="../assets/js/theme.js"></script>
